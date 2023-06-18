588274374f76dec9e3b29f73be1e41a04ae34434
raassh-23 <muh.nur.abdurrauf@gmail.com>
2023-06-18 11:00:02 +0700

    add translation in menu

--- scripts/translations/en.js
+++ scripts/translations/en.js
@@ -2,6 +2,38 @@ const en = {
     translation: {
         'menu': {
             'version': 'Version {{version}}',
+            'leaderboard': {
+                'title': 'Leaderboard',
+                'level': 'Level',
+                'sort': 'Sort',
+                'name': 'Name',
+                'time': 'Time',
+                'steps': 'Steps',
+                'code-blocks': 'Blocks',
+                'date': 'Date',
+            },
+            'credits': {
+                'title': 'Credits',
+                'designer-programmer': 'Game Designer & Programmer',
+                'advisors': 'Advisors',
+                'graphics': 'Graphics',
+                'music-sfx': 'Music & Sound Effects',
+            },
+            'setting': {
+                'title': 'Setting',
+                'volume': 'Sounds Volume',
+                'fullscreen': 'Fullscreen',
+                'language': 'Language',
+                'reset': 'Reset Data',
+                'install': 'Install',
+                'installed': 'Installed',
+                'reset-confirm': {
+                    'title': 'Are you sure?',
+                    'subtitle': 'All your progress will be lost.',
+                    'yes': 'Reset',
+                    'no': 'Cancel',
+                }
+            },
         }
     }
 };
--- scripts/translations/id.js
+++ scripts/translations/id.js
@@ -2,7 +2,39 @@ const id = {
     translation: {
         'menu': {
             'version': 'Versi {{version}}',
-        }
+            'leaderboard': {
+                'title': 'Papan Peringkat',
+                'level': 'Level',
+                'sort': 'Urutkan',
+                'name': 'Nama',
+                'time': 'Waktu',
+                'steps': 'Langkah',
+                'code-blocks': 'Blok',
+                'date': 'Tanggal',
+            },
+            'credits': {
+                'title': 'Kredit',
+                'designer-programmer': 'Desainer & Programmer Game',
+                'advisors': 'Pembimbing',
+                'graphics': 'Grafis',
+                'music-sfx': 'Musik & Efek Suara',
+            },
+            'setting': {
+                'title': 'Pengaturan',
+                'volume': 'Volume Suara',
+                'fullscreen': 'Layar Penuh',
+                'language': 'Bahasa',
+                'reset': 'Reset Data',
+                'install': 'Install',
+                'installed': 'Terinstall',
+                'reset-confirm': {
+                    'title': 'Apa kamu yakin?',
+                    'subtitle': 'Semua progress kamu akan hilang.',
+                    'yes': 'Reset',
+                    'no': 'Batal',
+                }
+            },
+        },
     }
 };
 
